f2cc7f15b37b331619b3a69cb2fb3962509b5111
xinyuan.yang <xinyuan.yang@vhall.com>
2021-12-24 20:17:00 +0800

    feat: $middleEventSdk采用CDN

--- src/demo/live-pc/main.js
+++ src/demo/live-pc/main.js
@@ -4,11 +4,17 @@ import App from './App.vue';
 import './config';
 import { initI18n } from '@/packages/locales/index.js';
 import router from './router';
+<<<<<<< HEAD
 import platformsSDK from '@/core';
+=======
+// element-ui 样式重置
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import './assets/icon-font/iconfont.css';
+>>>>>>> fe446b1 (feat: $middleEventSdk采用CDN)
 
 Vue.use(ElementUI);
 Vue.config.productionTip = false;
-Vue.use(platformsSDK);
 
 // 挂载i18n
 const i18n = initI18n('zh');
