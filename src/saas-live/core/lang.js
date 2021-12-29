8054360308707788927f06002f8a3f4bce8b39a0
xinyuan.yang <xinyuan.yang@vhall.com>
2021-12-29 18:29:21 +0800

    Dev

--- src/saas-live/core/lang.js
+++ src/saas-live/core/lang.js
@@ -7,10 +7,9 @@ Vue.use(VueI18n);
  * 国际化语言，初始化i18n
  * @param {*} locale
  */
-export function initI18n(lang = 'zh') {
-  const lan = localStorage.getItem('lang');
+export function initI18n() {
   const i18n = new VueI18n({
-    locale: lan || window.$globalConfig?.lang || lang,
+    locale: window.$globalConfig.currentLang,
     messages: {
       zh: require('../languages/zh-CN.js'),
       en: require('../languages/en-US.js')
