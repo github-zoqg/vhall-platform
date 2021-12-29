4c1baa2b34622acefa5d3230ab288485b0ab771e
xinyuan.yang <xinyuan.yang@vhall.com>
2021-12-29 17:04:24 +0800

    feat: 引入第三方剪切板组件

--- src/saas-live/core/config.js
+++ src/saas-live/core/config.js
@@ -1,7 +1,9 @@
 import Vue from 'vue';
 import { globalConfig, serverConfig } from '../config';
+import clipboard from 'clipboard';
 
 export function initConfig() {
   window.$globalConfig = Vue.prototype.$globalConfig = globalConfig;
   window.$serverConfig = Vue.prototype.$serverConfig = serverConfig;
+  Vue.prototype.$clipboard = clipboard;
 }
