b5a499686173617ec20a8a7292b70e005e9eefc9
xinyuan.yang <xinyuan.yang@vhall.com>
2022-01-04 10:17:18 +0800

    feat: 在packages中新增app-shared目录存放app使用的公共资源

--- src/saas-watch/core/mixin.js
+++ src/saas-watch/core/mixin.js
@@ -1,5 +1,5 @@
 import Vue from 'vue';
-import serverRegisterMixin from '@/packages/mixins/server-register';
+import serverRegisterMixin from '@/packages/app-shared/mixins/server-register';
 
 export function initMixin() {
   // 全局混入注册逻辑
--- src/zhike-live/core/use.js
+++ src/zhike-live/core/use.js
@@ -1,8 +1,4 @@
 import Vue from 'vue';
-// element-ui
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-// import '../assets/icon-font/iconfont.css';
 // 组件库组件
 import VmpContainer from '@/packages/container';
 import VmpAirContainer from '@/packages/air-container';
@@ -23,7 +19,6 @@ import VmpUserSettingBox from '@/packages/user-setting-box';
 
 // 全局注册组件
 export function initUse() {
-  Vue.use(ElementUI);
   Vue.use(VmpContainer);
   Vue.use(VmpAirContainer);
   Vue.use(VmpAsideMenu);
