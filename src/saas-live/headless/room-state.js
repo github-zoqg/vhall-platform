3aae24ff63e4fe72ef4ec5eebe3212108f8bb51f
xinyuan.yang <xinyuan.yang@vhall.com>
2022-01-11 18:18:01 +0800

    feat: domain state响应式处理

--- src/saas-live/headless/room-state.js
+++ src/saas-live/headless/room-state.js
@@ -3,17 +3,8 @@ import { useMsgServer, useRoomBaseServer, useDocServer, useInteractiveServer } f
 export default async function () {
   console.log('%c------服务初始化 开始', 'color:blue');
   const msgServer = useMsgServer();
-
-  console.log('%c------服务初始化 msgServer 初始化完成', 'color:blue');
-
   const docServer = useDocServer();
-
-  console.log('%c------服务初始化 docServer 初始化完成', 'color:blue');
-
   const interactiveServer = useInteractiveServer();
-
-  console.log('%c------服务初始化 interactiveServer 初始化完成', 'color:blue');
-
   const roomBaseServer = useRoomBaseServer();
 
   if (!roomBaseServer) {
@@ -22,9 +13,11 @@ export default async function () {
   console.log('%c------服务初始化 roomBaseServer 初始化完成', 'color:blue');
 
   await msgServer.init();
-  await interactiveServer.init();
+  console.log('%c------服务初始化 msgServer 初始化完成', 'color:blue', msgServer);
 
-  await docServer.init({
-    token: roomBaseServer.state.watchInitData.interact.paas_access_token
-  });
+  await interactiveServer.init();
+  console.log('%c------服务初始化 interactiveServer 初始化完成', 'color:blue');
+
+  await docServer.init();
+  console.log('%c------服务初始化 docServer 初始化完成', 'color:blue', docServer);
 }
