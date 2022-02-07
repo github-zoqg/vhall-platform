504c858a9c2a423516b60bfc3df18473b6f3e2e4
yongkai.shao <yongkai.shao@vhall.com>
2022-02-07 12:17:17 +0800

    feat: 添加知客发起端

--- src/zhike-live/webpack.dev.config.js
+++ src/zhike-live/webpack.dev.config.js
@@ -1,4 +1,21 @@
 const _ = require('lodash');
 const baseConfig = require('./webpack.base.config');
 
-module.exports = _.merge({}, baseConfig);
+const htmlConfig = {
+  // cdn css
+  cdnCss: {
+    // iconfontCommon: '//t-alistatic01.e.vhall.com/common-static/middle/iconfont/vh-pc/iconfont.css',
+    iconfont: '//t-alistatic01.e.vhall.com/common-static/middle/iconfont/vh-zk-pc/iconfont.css'
+  }
+};
+
+module.exports = _.merge(
+  {
+    pages: {
+      index: {
+        ...htmlConfig
+      }
+    }
+  },
+  baseConfig
+);
