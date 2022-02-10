4587d868c0c646df3001accf0553f18fe9f003fb
xinyuan.yang <xinyuan.yang@vhall.com>
2022-02-10 23:01:43 +0800

    Feature 1.0.0

--- src/packages/group-discussion/src/group-add.vue
+++ src/packages/group-discussion/src/group-add.vue
@@ -19,7 +19,9 @@
 
       <!-- 底部按钮 -->
       <div slot="footer" class="vmp-group-ft">
-        <el-button type="primary" :round="true" @click="handleSubmit">确定</el-button>
+        <el-button type="primary" :round="true" @click.native="handleThrottleSubmit()">
+          确定
+        </el-button>
         <el-button :round="true" @click="handleClose">取消</el-button>
       </div>
     </el-dialog>
@@ -27,6 +29,8 @@
 </template>
 <script>
   import { useGroupServer } from 'middle-domain';
+  import _ from 'lodash';
+
   export default {
     name: 'VmpGroupAdd',
     props: {
@@ -53,6 +57,9 @@
       show: function (newVal) {
         this.dialogVisible = newVal;
       }
+    },
+    mounted() {
+      this.handleThrottleSubmit = _.throttle(this.handleSubmit, 300, { trailing: false });
     },
     methods: {
       // 新增分组确定
