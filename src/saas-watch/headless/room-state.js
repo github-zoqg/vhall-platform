901e5b9f50a515df1f7c1261b3dbc50d46511226
jia.li <jia.li@vhall.com>
2022-02-11 11:27:20 +0800

    feat: 观看页调用get-config-list & 黄金链路。

--- src/saas-watch/headless/room-state.js
+++ src/saas-watch/headless/room-state.js
@@ -20,10 +20,20 @@ export default async function () {
   if (!roomBaseServer) {
     throw Error('get roomBaseServer exception');
   }
+  // TODO：晓东确认，是否在此处添加，配置项调用
+  await roomBaseServer.getConfigList();
+  await roomBaseServer.getLowerConfigList({
+    params: {},
+    environment: process.env.NODE_ENV != 'production' ? 'test' : 'production',
+    systemKey: 2,
+    time: 5
+  });
+  console.log('%c------黄金链路请求配置项完成', 'color:pink');
+  console.log(roomBaseServer.state.configList);
   // TODO 设置观看端测试权限数据
-  roomBaseServer.state.configList = {
-    hasToolbar: false
-  };
+  // roomBaseServer.state.configList = {
+  //   hasToolbar: false
+  // };
 
   await msgServer.init();
   console.log('%c------服务初始化 msgServer 初始化完成', 'color:blue');
