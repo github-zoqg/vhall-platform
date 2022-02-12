ccd0704e1195a4fa6dd023bdf271f0b495a2f34e
xiao.zhang <xiao.zhang@vhall.com>
2022-02-12 11:25:04 +0800

    修改互动图标

--- src/saas-watch/headless/room-state.js
+++ src/saas-watch/headless/room-state.js
@@ -31,9 +31,28 @@ export default async function () {
   console.log('%c------黄金链路请求配置项完成', 'color:pink');
   console.log(roomBaseServer.state.configList);
   // TODO 设置观看端测试权限数据
-  // roomBaseServer.state.configList = {
-  //   hasToolbar: false
-  // };
+  roomBaseServer.state.configList = {
+    hasToolbar: false
+  };
+  // 调用聚合接口
+  roomBaseServer.getCommonConfig({
+    tags: [
+      'skin',
+      'screen-poster',
+      'like',
+      'keywords',
+      'public-account',
+      'webinar-tag',
+      'menu',
+      'adv-default',
+      'invite-card',
+      'red-packet',
+      'room-tool',
+      'goods-default',
+      'announcement',
+      'sign'
+    ]
+  });
 
   await msgServer.init();
   console.log('%c------服务初始化 msgServer 初始化完成', 'color:blue');
