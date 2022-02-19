b277a37e9d5da5dd47509f072eb39ef461cf127d
xinyuan.yang <xinyuan.yang@vhall.com>
2022-02-19 22:06:32 +0800

    chore: 更新图标

--- src/saas-watch/webpack.dev.config.js
+++ src/saas-watch/webpack.dev.config.js
@@ -2,7 +2,13 @@ const _ = require('lodash');
 const baseConfig = require('./webpack.base.config');
 
 const htmlConfig = {
-  cdnJs: {}
+  cdnJs: {},
+  cdnCss: {
+    iconfontCommon:
+      '//t-alistatic01.e.vhall.com/common-static/middle/iconfont/vh-pc/v1.0.1/iconfont.css',
+    iconfont:
+      '//t-alistatic01.e.vhall.com/common-static/middle/iconfont/vh-saas-pc/v1.0.1/iconfont.css'
+  }
 };
 
 module.exports = _.merge(baseConfig, {
