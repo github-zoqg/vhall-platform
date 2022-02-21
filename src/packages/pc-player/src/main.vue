6d3db391e781ba02bf93ccb38f54efa6e2daabd0
zhangxiao <215906340>
2022-02-21 23:02:55 +0800

    feat: 结束直播销毁播放器

--- src/packages/pc-player/src/main.vue
+++ src/packages/pc-player/src/main.vue
@@ -24,7 +24,7 @@
               <i class="vh-iconfont vh-line-video-play"></i>
             </div>
           </div>
-          <div class="vmp-player-living-end" v-if="isLivingEnd">
+          <!-- <div class="vmp-player-living-end" v-if="isLivingEnd">
             <div
               :class="
                 displayMode == 'mini' ? 'vmp-player-living-end-mini' : 'vmp-player-living-end-img'
@@ -33,7 +33,7 @@
               <img src="../src/images/liveEnd.png" alt="" />
             </div>
             <h1>直播已结束</h1>
-          </div>
+          </div> -->
           <div class="vmp-player-living-vodend" v-if="isVodEnd">
             <div class="vmp-player-living-vodend-try" v-if="isTryPreview">
               <h3>{{ $t('appointment.appointment_1013') }}</h3>
