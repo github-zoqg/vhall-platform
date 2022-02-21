a67024d7e0c17c79a64e3974378652ec293065f1
ningkang.he <ningkang.he@vhall.com>
2022-02-21 15:39:14 +0800

    feat: watch端配置缺失补充

--- src/saas-watch/config.js
+++ src/saas-watch/config.js
@@ -45,7 +45,7 @@ export const serverConfig = {
   },
   layerBodyRight: {
     component: 'VmpBasicRightContainer',
-    children: ['comChat']
+    children: ['comTabMenu']
   },
   /*** 布局定义end */
 
@@ -67,7 +67,33 @@ export const serverConfig = {
     ]
   },
 
+  comTabMenu: {
+    component: 'VmpTabMenu',
+    options: {
+      /**
+       * 菜单配置不是最终的显示，而是较全的配置表，具体显示要结合接口具体给过来哪些数据
+       * 此配置主要涉及到type对应哪个cuid
+       */
+      menuConfig: [
+        { type: 1, cuid: 'comCustomMenu', text: '' }, //自定义菜单
+        { type: 2, cuid: 'comDoc', text: 'menu.menu_1001', visible: false }, // 文档(默认隐藏)
+        { type: 3, cuid: 'comChat', text: 'menu.menu_1002' }, // 聊天
+        { type: 'notice', cuid: 'comNotice', text: '公告' },
+        // { type: 4, cuid: 'comIntro', text: 'menu.menu_1003' }, // 简介
+        { type: 5, cuid: 'comGoodSaas', text: 'menu.menu_1004' }, // 商品
+        { type: 6, cuid: 'comRecommend', text: 'menu.menu_1005' }, // 广告、推荐
+        { type: 7, cuid: 'comChapter', text: 'menu.menu_1013' }, // 章节
+        { type: 8, cuid: 'comMemberList', text: '成员' } // 成员
+      ]
+    }
+  },
+
   /**** 组件定义 */
+  // 自定义菜单
+  comCustomMenu: {
+    component: 'VmpCustomMenu'
+  },
+
   // 文档白板组件
   comDocUne: {
     component: 'VmpDocUne',
@@ -280,5 +306,9 @@ export const serverConfig = {
       method: 'changePlayTime',
       args: ['$0']
     }
+  },
+  // 推荐
+  comRecommend: {
+    component: 'VmpRecommend'
   }
 };
