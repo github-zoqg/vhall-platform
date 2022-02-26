c4777b9c3a13e75d53d5bfe859614e95168b9f6f
kai.liu <xiao.zhang@vhall.com>
2022-02-26 17:44:51 +0800

    添加wap端桌面共享

--- src/packages/stream-desktop-screen-wap/src/main.vue
+++ src/packages/stream-desktop-screen-wap/src/main.vue
@@ -1,7 +1,6 @@
 <template>
-  <div class="vmp-desktop-screen">
-    <!-- 推桌面共享容器 -->
-      fffffffffffffffffffffffffffffffffff
+  <div id="vmp-wap-desktop-screen" v-show="isShowWapDesktopScreen">
+    <!-- 订阅桌面共享容器 -->
   </div>
 </template>
 
@@ -18,6 +17,8 @@
     name: 'VmpWapDesktopScreen',
     data() {
       return {
+        streamId: null, // 桌面共享流Id
+        isShowWapDesktopScreen:false, // wap端是否显示桌面共享容器
         popAlert: {
           text: '请确认开启桌面共享，为了体验更好，建议选择“您的整个屏幕”',
           visible: false,
@@ -47,124 +48,56 @@
       this.streamId = '';
       this.addEvents();
     },
-    mounted() {},
-    watch: {
-      isShareScreen(newval) {
-        this.roomBaseServer.setShareScreenStatus(newval);
-      },
-      ['roomBaseServer.state.miniElement'](newval) {
-        console.log('-[doc][player]---大小屏变更miniElement：', newval); // newval 取值 doc, stream-list
-        const mode = newval === 'doc' ? 'mini' : 'normal';
-        // this.setDisplayMode(mode);
+    mounted() {
+      // 刷新或者上麦 重新订阅
+      let stream = this.interactiveServer.getDesktopAndIntercutInfo();
+      if (stream?.streamType === 3) {
+        this.subscribeDesktopScreen(stream.streamId);
       }
     },
     methods: {
-      addEvents() {
-        // 桌面共享 流结束
-        this.interactiveServer.on(VhallRTC.EVENT_REMOTESTREAM_REMOVED, e => {
-          if (e.data.streamId == this.streamId) {
-            this.streamId = '';
-            this.isShareScreen = false;
-          }
+      // 订阅桌面共享流
+      subscribeDesktopScreen(streamId) {
+      //订阅时展示桌面共享容器
+      this.isShowWapDesktopScreen = true
+      //存一下当前的流Id，刷新后使用
+      sessionStorage.setItem('currentDesktopStreamID',streamId)
+      const opt = {
+          streamId,
+          videoNode: 'vmp-wap-desktop-screen', // 远端流显示容器，必填
+          mute: { audio: false, video: false } // 是否静音，关视频。选填 默认false
+        };
+        this.desktopShareServer.subscribeDesktopShareStream(opt).then(() => {
+          useRoomBaseServer().setShareScreenStatus(true);
         });
       },
-      showConfirm() {
-        this.popAlert.visible = true;
-      },
-
-      // 开始共享
-      startShare() {
-        this.closeConfirm();
-        const configuredProfile = this.mediaSettingServer.state.screenRate;
-        let profile = VhallRTC.RTC_SCREEN_PROFILE_1080P_16x9_M;
-        if (configuredProfile) {
-          profile = VhallRTC[configuredProfile];
-        }
-        const options = {
-          videoNode: 'vmp-desktop-screen-container', // 传入本地视频显示容器，必填
-          profile // 自定义分辨率，使用分辨率模板，选填，与videoQuality参数互斥，优先使用profile参数，推荐使用。
-        };
-
-        this.desktopShareServer
-          .startShareScreen(options)
-          .then(data => {
-            this.streamId = data.streamId;
-            this.desktopShareServer
-              .publishDesktopShareStream(data.streamId)
-              .then(() => {
-                const {
-                  state: {
-                    watchInitData: {
-                      join_info: { role_name }
-                    }
-                  }
-                } = this.roomBaseServer;
-
-                if (role_name == 1 && !this.isInGroup) {
-                  this.interactiveServer.setBroadCastScreen({ mainScreenStreamId: data.streamId });
-                  this.interactiveServer.setBroadCastLayout({
-                    layout: VhallRTC.CANVAS_LAYOUT_PATTERN_GRID_1
-                  });
-                }
-
-                this.isShareScreen = true;
-                console.log('[screen] 桌面共享推流成功');
-              })
-              .catch(error => {
-                console.log(error, '推流失败');
-              });
-          })
-          .catch(error => {
-            console.error('[screen] 桌面共享创建本地流失败', error);
-            if (error.data.error.type == 'access-denied') {
-              if (/macintosh|mac os x/i.test(navigator.userAgent)) {
-                this.isShowAccessDeniedAlert = true;
-              }
-            }
-          });
-      },
-      // 关闭弹窗
-      closeConfirm() {
-        this.popAlert.visible = false;
-      },
-
-      // 全屏
-      fullScreen(el) {
-        this.isFullscreen = !this.isFullscreen;
-        if (this.isFullscreen) {
-          this.enterFullscreen(el);
-        } else {
-          this.exitFullscreen(el);
-        }
-      },
-      enterFullscreen() {
-        const element = document.querySelector('.vmp-basic-container');
-        if (!this.assistantType) {
-          if (element.requestFullscreen) element.requestFullscreen();
-          else if (element.mozRequestFullScreen) element.mozRequestFullScreen();
-          else if (element.webkitRequestFullscreen) {
-            element.webkitRequestFullscreen();
-          } else if (element.msRequestFullscreen) element.msRequestFullscreen();
-        }
-      },
-      exitFullscreen() {
-        if (!this.assistantType) {
-          if (document.exitFullscreen) document.exitFullscreen();
-          else if (document.mozCancelFullScreen) document.mozCancelFullScreen();
-          else if (document.webkitExitFullscreen) document.webkitExitFullscreen();
-          else if (document.msExitFullscreen) document.msExitFullscreen();
-        }
+      addEvents() {
+        // 监听桌面共享流加入
+        this.desktopShareServer.$on('screen_stream_add', e => {
+          this.streamId = e.data.streamId
+          this.subscribeDesktopScreen(e.data.streamId);
+        });
+        // 监听桌面共享流离开
+        this.desktopShareServer.$on('screen_stream_remove', e => {
+          // ()中的逻辑，如果刷新之后，没有streamId的值，需要从sessionStorage中取当前的流Id
+          if (e.data.streamId == (this.streamId || sessionStorage.getItem('currentDesktopStreamID'))) {
+            this.streamId = '';
+            this.isShowWapDesktopScreen = false;
+          }
+        });
       }
     }
   };
 </script>
 
 <style lang="less">
-  .vmp-desktop-screen {
+#vmp-wap-desktop-screen {
     width: 100%;
     height: 100%;
     background: #2d2d2d;
-    position: relative;
+    position: absolute;
+    top: 0;
+    left: 0;
     &__tip {
       text-align: center;
       position: absolute;
