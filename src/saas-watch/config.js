9e66c464fde895ff6515b55f0e52397042861f63
ningkang.he <ningkang.he@vhall.com>
2022-03-01 16:17:51 +0800

    feat: 先默认打开toggleBtnVisible,分场景区分显隐

--- src/saas-watch/config.js
+++ src/saas-watch/config.js
@@ -79,7 +79,7 @@ export const serverConfig = {
     component: 'VmpTabMenu',
     options: {
       // 是否展示左右按钮
-      isToggleBtnVisible: false,
+      isToggleBtnVisible: true,
       /**
        * 菜单配置不是最终的显示，而是较全的配置表，具体显示要结合接口具体给过来哪些数据
        * 此配置主要涉及到type对应哪个cuid
