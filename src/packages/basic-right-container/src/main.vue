7ec0e61c44663bbea89caeb0225a84922c0ba76c
junyuan.cao <junyuan.cao@vhall.com>
2022-03-07 15:44:58 +0800

    fix: v-for改key值

--- src/packages/basic-right-container/src/main.vue
+++ src/packages/basic-right-container/src/main.vue
@@ -30,8 +30,8 @@
         // TODO:后续添加插播桌面共享后，再添加插播桌面共享场景的处理
         return (
           this.$domainStore.state.docServer.switchStatus ||
-          this.desktopShareServer.state.isShareScreen ||
-          !this.live_over
+          this.desktopShareServer.state.isShareScreen
+          // !this.live_over
         );
       },
       isTryWatch() {
