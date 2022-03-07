b492ba3dab1f02ea85f554f9acc2fecb889d9801
xiaodong.ding <xiaodong.ding@vhall.com>
2022-03-07 19:53:09 +0800

    fix: 助理和嘉宾被踢出

--- src/saas-live/views/Home.vue
+++ src/saas-live/views/Home.vue
@@ -77,6 +77,7 @@
         }
         console.log('%c---初始化直播房间 完成', 'color:blue');
         this.state = 1;
+        this.addEventListener();
       } catch (err) {
         console.error('---初始化直播房间出现异常--');
         console.error(err);
@@ -112,6 +113,16 @@
             email
           }
         });
+      },
+      addEventListener() {
+        const roomBaseServer = useRoomBaseServer();
+        roomBaseServer.$on('ROOM_KICKOUT', () => {
+          this.handleKickout();
+        });
+      },
+      handleKickout() {
+        this.state = 2;
+        this.errMsg = '您已被禁止访问当前活动';
       }
     }
   };
