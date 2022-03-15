9d2784fbdf56908229a957ebd3695a11e28752ce
ningkang.he <ningkang.he@vhall.com>
2022-03-15 18:19:55 +0800

    fix: media-setting的basic-setting调整

--- src/packages/media-setting/src/components/pages/basic-setting.vue
+++ src/packages/media-setting/src/components/pages/basic-setting.vue
@@ -163,10 +163,7 @@
         const saveScreenRate =
           sessionStorage.getItem('selectedScreenRate') || this.screenRatesConfig[1].value; // 默认PPT静态
 
-        const defaultLayout = this.filterLayoutConfig.find(
-          el => el.id === 'CANVAS_ADAPTIVE_LAYOUT_TILED_MODE'
-        );
-        const savedLayout = sessionStorage.getItem('layout') || defaultLayout.id; // 默认主次平铺
+        const savedLayout = sessionStorage.getItem('layout') || 'CANVAS_ADAPTIVE_LAYOUT_TILED_MODE'; // 默认主次平铺
 
         this.mediaState.rate = saveRate;
         this.mediaState.screenRate = saveScreenRate;
