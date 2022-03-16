4ba55195853f38ada55dd2b077624cfc1375090a
enlong.zhang <enlong.zhang@vhall.com>
2022-03-16 21:06:56 +0800

    feat: 聊天删除bug

--- src/packages/chat/src/components/msg-item.vue
+++ src/packages/chat/src/components/msg-item.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="vmp-chat-msg-item" v-if="!(isOnlyShowSponsor && source.roleName != 1)">
+    {{ source.msgId }}
     <!--消息发送时间-->
     <div v-if="showTime" class="vmp-chat-msg-item__showtime">{{ showTime }}</div>
     <!--常规消息-->
