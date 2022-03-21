60c5986dcb3f4374fb802ef2ec83da5ad2cbb029
renbeihong <beihongren@163.com>
2022-03-21 16:53:00 +0800

    fix: 设备异常提示

--- src/packages/wap-stream-local/src/main.vue
+++ src/packages/wap-stream-local/src/main.vue
@@ -55,6 +55,8 @@
   } from 'middle-domain';
   import { calculateAudioLevel, calculateNetworkStatus } from '../../app-shared/utils/stream-utils';
   import { boxEventOpitons } from '@/packages/app-shared/utils/tool';
+  import { Dialog } from 'vant';
+
   export default {
     name: 'VmpWapStreamLocal',
     data() {
@@ -169,6 +171,14 @@
     },
     mounted() {
       this.checkStartPush();
+
+      // 监听设备禁用
+      useInteractiveServer().$on('EVENT_STREAM_END', msg => {
+        Dialog.alert({
+          title: this.$t('account.account_1061'),
+          message: this.$t('interact.interact_1011')
+        });
+      });
     },
     async beforeDestroy() {
       // 清空计时器
