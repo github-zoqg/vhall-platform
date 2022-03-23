36207f8d61b3c685a1b8f13a47d17744e905a730
zhangxiao <215906340>
2022-03-23 13:44:21 +0800

    feat: 添加默认图

--- src/packages/live-private-chat/src/components/chat-list.vue
+++ src/packages/live-private-chat/src/components/chat-list.vue
@@ -79,8 +79,8 @@
       </li>
     </ul>
     <dl class="private-chat__empty" v-else-if="finishData">
-      <!-- <dt></dt>
-      <dd>暂时没有聊天哦～</dd> -->
+      <dt></dt>
+      <dd>暂时没有聊天哦～</dd>
     </dl>
   </div>
 </template>
@@ -417,7 +417,7 @@
       dt {
         width: 130px;
         height: 130px;
-        background: url(../images/no-data.png) no-repeat center;
+        background: url(../images/no-create.png) no-repeat center;
         border-radius: 50%;
         background-size: contain;
       }
