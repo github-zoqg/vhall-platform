e01cf8ca8a70bb7160ad7c7bc736d3cf5da9fbc6
chencheng.zeng <chencheng.zeng@vhall.com>
2022-03-28 11:38:51 +0800

    feat:聊天、成员列表，增加数据上报埋点

--- src/packages/member-list/src/main.vue
+++ src/packages/member-list/src/main.vue
@@ -1482,6 +1482,8 @@
         if (this.tabIndex === 3) {
           this.switchToTab(3);
         }
+        //数据埋点--刷新成员列表
+        window.vhallReportForProduct?.report(110134);
         this.pageConfig.page = 0;
         this.searchUserInput = '';
         this.getOnlineUserList();
@@ -1510,7 +1512,8 @@
           .setHandsUp(params)
           .then(res => {
             console.log('switch-mic-status', res);
-            //todo 上报埋点
+            //数据埋点--开启/关闭允许举手
+            window.vhallReportForProduct?.report(element.target.checked ? 110127 : 110128);
             this.$message.success({ message: '设置成功' });
           })
           .catch(err => {
@@ -1553,7 +1556,8 @@
         if (['', null, void 0].includes(this.searchUserInput)) {
           return;
         }
-        //todo 埋点上报
+        // 数据埋点-搜索人员
+        window.vhallReportForProduct?.report(110129);
         this.pageConfig.page = 0;
         this.getOnlineUserList();
       },
@@ -1784,6 +1788,8 @@
               if (!['', null, void 0].includes(accountId) && accountId === this.userId) {
                 // this.$message.success('邀请演示发送成功')
               } else {
+                //数据埋点--邀请上麦
+                window.vhallReportForProduct?.report(110130);
                 this.$message.success(this.$t('message.message_1033'));
               }
             } else {
@@ -1829,7 +1835,8 @@
         };
         mutedUser(params)
           .then(res => {
-            //todo 事件上报
+            //数据埋点--禁言/取消禁言
+            window.vhallReportForProduct?.report(110125);
             console.warn('禁言---res', res);
           })
           .catch(err => {
@@ -1878,7 +1885,8 @@
                 return this.$message.error(msg);
               }
               // 踢出只能在在线和举手列表操作
-              //todo 上报事件
+              //埋点上报--踢出/取消踢出
+              window.vhallReportForProduct?.report(nextStatus ? 110126 : 110146);
               if (nextStatus) {
                 this._deleteUser(accountId, this.onlineUsers, 'onlineUsers');
                 this._deleteUser(accountId, this.applyUsers, 'applyUsers');
