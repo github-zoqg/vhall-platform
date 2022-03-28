4e68777804300941c68431fbab09ef17116681c8
ningkang.he <ningkang.he@vhall.com>
2022-03-28 14:08:41 +0800

    feat: media-setting 埋点 + stream注释补充

--- src/packages/stream-remote/src/main.vue
+++ src/packages/stream-remote/src/main.vue
@@ -291,6 +291,7 @@
           return this.$domainStore.state.roomBaseServer.interactToolStatus.main_screen;
         }
       },
+      // 文档演示者的ID
       presentationScreen() {
         if (this.isInGroup) {
           return this.$domainStore.state.groupServer.groupInitData.presentation_screen;
@@ -301,12 +302,12 @@
       //显示是否在演示中
       isShowPresentationScreen() {
         const { accountId } = this.stream;
-        const sameId = this.presentationScreen === accountId;
-        const groupMode = this.liveMode == 6;
-        const inMainRoomUser = !this.isInGroup && accountId != this.hostId;
-        const inGroupRoomUser = this.isInGroup && accountId != this.groupLeaderId;
-        const allowedUser = inMainRoomUser || inGroupRoomUser;
-        return sameId && groupMode && allowedUser;
+        const sameId = this.presentationScreen === accountId; // 演示者ID为当前流的用户ID
+        const isGroupMode = this.liveMode == 6; // 分组类型
+        const inMainRoomUser = !this.isInGroup && accountId != this.hostId; // 在主房间且不是主持人
+        const inGroupRoomUser = this.isInGroup && accountId != this.groupLeaderId; // 在分组房间且不是组长
+        const allowedUser = inMainRoomUser || inGroupRoomUser; // 普通用户
+        return sameId && isGroupMode && allowedUser;
       },
       joinInfo() {
         return this.$domainStore.state.roomBaseServer.watchInitData.join_info;
