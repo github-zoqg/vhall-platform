c3eaf71a251b629a348daecaab1c807d79a2aeb8
zhangxiao <215906340>
2022-03-29 21:16:59 +0800

    feat: 修改嵌入

--- src/packages/pc-player/src/main.vue
+++ src/packages/pc-player/src/main.vue
@@ -3,7 +3,11 @@
     v-if="!isShowContainer"
     :class="[
       'vmp-player',
-      { 'is-watch': isWatch, 'vmp-player-embed': isEmbedVideo, 'vmp-player-embedFull': isEmbed },
+      {
+        'is-watch': isWatch,
+        'vmp-player-embed': isEmbedVideo,
+        'vmp-player-embedFull': isEmbed && !isEmbedVideo
+      },
       isSubscribe ? '' : `vmp-player--${displayMode}`
     ]"
     @mousemove="wrapEnter"
@@ -94,7 +98,7 @@
         <div
           class="vmp-player-living-exchange"
           @click="exchangeVideoDocs"
-          v-if="isVisibleMiniElement && hoveVideo && !isVodEnd"
+          v-if="isVisibleMiniElement && hoveVideo && !isVodEnd && !isEmbedVideo"
         >
           <p>
             <el-tooltip :content="$t('player.player_1008')" placement="top">
