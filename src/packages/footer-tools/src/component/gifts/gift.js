95191e80509096b91de1a52a2b5863374b03cb39
yongkai.shao <yongkai.shao@vhall.com>
2022-04-05 20:44:33 +0800

    fix: 送礼物头像问题

--- src/packages/footer-tools/src/component/gifts/gift.js
+++ src/packages/footer-tools/src/component/gifts/gift.js
@@ -143,10 +143,10 @@ export default {
           avatar: this.watchInitData.join_info.avatar,
           barrageTxt: '',
           text_content: '',
-          gift_user_nickname: this.watchInitData.join_info.nickname,
+          nickname: this.watchInitData.join_info.nickname,
           role_name: 2,
           gift_name: this.giftInfo.name,
-          gift_image_url: this.giftInfo.image_url,
+          gift_url: this.giftInfo.image_url,
           source_status: this.giftInfo.source_status
         };
         const context = {
