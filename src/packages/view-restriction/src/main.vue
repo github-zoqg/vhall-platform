55bff479a7b06b70631b58c93abb62b3b6b32b93
zhiqiang.hao <zhiqiang.hao@vhall.com>
2022-04-12 11:39:57 +0800

    fix: 修改观看限制

--- src/packages/view-restriction/src/main.vue
+++ src/packages/view-restriction/src/main.vue
@@ -1,23 +1,25 @@
 <template>
   <div
-    v-if="showRestriction && restrictionOpts"
-    :class="['vmp-view-restriction', { 'force-agree': !restrictionOpts.rule }]"
+    v-if="agreementPopupVisible && agreementInfo.rule"
+    :class="['vmp-view-restriction', { 'force-agree': !agreementInfo.rule }]"
   >
     <div class="vmp-view-restriction-wrap">
       <div class="restriction-title">
-        {{ restrictionOpts.title }}
+        {{ agreementInfo.title }}
       </div>
       <div
-        :class="['restriction-content', { 'more-content': restrictionOpts.rule == 1 }]"
-        v-html="restrictionOpts.content"
+        :class="['restriction-content', { 'more-content': agreementInfo.rule == 1 }]"
+        v-html="agreementInfo.content"
       ></div>
-      <div class="restriction-law" v-html="restrictionOpts.statement_content"></div>
-      <div v-if="restrictionOpts.statement_status" class="restriction-control">
-        <template v-if="!restrictionOpts.rule">
-          <span @click.stop="close(0)">{{ $t('webinar.webinar_1044') }}</span>
-          <span @click.stop="close(-1)">{{ $t('webinar.webinar_1045') }}</span>
+      <div class="restriction-law" v-html="agreementInfo.statement_content"></div>
+      <div v-if="agreementInfo.statement_status" class="restriction-control">
+        <template v-if="!agreementInfo.rule">
+          <span @click.stop="agree">{{ $t('webinar.webinar_1044') }}</span>
+          <span @click.stop="disagree">{{ $t('webinar.webinar_1045') }}</span>
         </template>
-        <span v-else @click.stop="close(1)">{{ $t('webinar.webinar_1046') }}</span>
+        <span v-else @click.stop="agreementPopupVisible = false">
+          {{ $t('webinar.webinar_1046') }}
+        </span>
       </div>
     </div>
   </div>
@@ -29,78 +31,37 @@
     name: 'VmpViewRestriction',
     data() {
       return {
-        showRestriction: true,
-        restrictionOpts: {
-          rule: 0, // rule: 0 强制同意  rule: 1 非强制同意
-          statement_status: 1, // 是否开启观看限制
-          title: '微吼直播观看协议', // 限制标题
-          content:
-            '3月25日，有网友称海南航空HU7350因机械故障出现延误。同日15时许，海航工作人员向记者表示，HU7350航班出现飞机故障并因此延误，目前已离港。 海南航空公司通报称，3月25日，海航HU7350重庆—三亚航班在起飞前因临时故障原因导致延误。为减少旅客等待时间，公司决策更换飞机。目前，该航班已于当日16时52分安全抵达三亚凤凰国际机场。 <img src="https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fup.enterdesk.com%2F2021%2Fedpic_360_360%2Ffe%2F24%2F6a%2Ffe246a72021e3dafc1afdce09bb25fed_1.jpg&refer=http%3A%2F%2Fup.enterdesk.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1652003401&t=2ffea4999a056a78b40b4f526c451df1" />3月25日，有网友称海南航空HU7350因机械故障出现延误。同日15时许，海航工作人员向记者表示，HU7350航班出现飞机故障并因此延误，目前已离港。 海南航空公司通报称，3月25日，海航HU7350重庆—三亚航班在起飞前因临时故障原因导致延误。为减少旅客等待时间，公司决策更换飞机。目前，该航班已于当日16时52分安全抵达三亚凤凰国际机场。', // 限制内容
-          is_open: 1, // 是否开启观看限制
-          statement_content: '你可以阅读《观看协议》了解详细信息',
-          statement_info: [
-            { title: '协议', link: 'http://www.baidu.com' },
-            { title: '观看', link: 'http://www.baidu.com' }
-          ]
-        } // 活动下观看限制信息
+        agreementPopupVisible: false, // 协议弹窗的显示
+        agreementInfo: {
+          // 协议信息
+          title: '',
+          content: '',
+          rule: 0, //协议规则 0:同意后进入 1:阅读后进入(每次进入直播间都弹出)
+          statement_content: '' // 协议声明
+        }
       };
     },
+    beforeCreate() {
+      this.roomServer = useRoomBaseServer();
+    },
     created() {
-      const roomServer = useRoomBaseServer();
-      this.restrictionOpts = roomServer.state.agreementInfo;
-      const { is_open, rule, statement_content, statement_info } = this.restrictionOpts;
-      const storeOpts = sessionStorage.getItem('restrictionStore')
-        ? JSON.parse(sessionStorage.getItem('restrictionStore'))
-        : [];
-      if (statement_info.length > 0) {
-        statement_info.forEach(state => {
-          const index = statement_content.indexOf(state.title);
-          this.restrictionOpts.statement_content =
-            statement_content.substring(0, index) +
-            `<a class="law-link" href=${state.link}>${state.title}</a>` +
-            statement_content.substring(index + state.title.length);
-        });
-      }
-
-      /**
-       * 观看限制弹窗展示逻辑
-       * 1.开启观看限制 is_open = 1
-       * 2.rule = 1 阅读后进入必弹 || rule = 0 需同意但未同意必弹
-       */
-      if (is_open == 1) {
-        if (!rule) {
-          if (storeOpts.length > 0 && storeOpts.includes(this.$route.params.id)) {
-            this.showRestriction = false;
-          } else {
-            this.showRestriction = true;
-          }
-        } else {
-          this.showRestriction = true;
-        }
-      } else {
-        this.showRestriction = false;
-      }
+      this.roomServer.$on('POPUP_AGREEMENT', this.handlePopupMsg);
+    },
+    destroyed() {
+      this.roomServer.$off('POPUP_AGREEMENT', this.handlePopupMsg);
     },
     methods: {
-      close(rule) {
-        const session = sessionStorage.getItem('restrictionStore')
-          ? JSON.parse(sessionStorage.getItem('restrictionStore'))
-          : [];
-        const webinarId = this.$route.params.id;
-        const bool = session.length > 0 ? session.includes(webinarId) : false;
-        if (rule < 0) {
-          // 时间派发 展示错误页面
-          const roomServer = useRoomBaseServer();
-          roomServer.$emit('VIEW_RESTRICTION_ERROR_PAGE');
-          return;
-        } else if (!rule) {
-          if (!bool) {
-            session.push(webinarId);
-          }
-          console.log(991, session, typeof session);
-          sessionStorage.setItem('restrictionStore', JSON.stringify(session));
-        }
-        this.showRestriction = false;
+      handlePopupMsg(payload) {
+        this.agreementInfo = payload;
+        this.agreementPopupVisible = true;
+      },
+      agree() {
+        this.roomServer.agreeWitthTerms();
+        this.agreementPopupVisible = false;
+      },
+      disagree() {
+        this.roomServer.refusesTerms();
+        this.agreementPopupVisible = false;
       }
     }
   };
