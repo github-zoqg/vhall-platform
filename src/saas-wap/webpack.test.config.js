1fdc39a4502529f9cea0ca520329b9ddafc3c517
jia.li <jia.li@vhall.com>
2022-04-15 10:23:46 +0800

    feat: 0415发版6.5.9-beta.6

--- src/saas-wap/webpack.test.config.js
+++ src/saas-wap/webpack.test.config.js
@@ -7,7 +7,7 @@ const htmlConfig = {
     MiddleEventSdk:
       '//t-alistatic01.e.vhall.com/common-static/middle/middle-event-sdk/0.3.1/index.js',
     MiddleDomain:
-      '//t-alistatic01.e.vhall.com/common-static/middle/middle-domain/6.5.9-beta.4/middle-domain.js'
+      '//t-alistatic01.e.vhall.com/common-static/middle/middle-domain/6.5.9-beta.6/middle-domain.js'
   }
 };
 
