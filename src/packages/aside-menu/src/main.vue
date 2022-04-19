371126302cfb270a4491c31e3b93efcc192c4bd7
xiaodong.ding <xiaodong.ding@vhall.com>
2022-04-19 20:20:30 +0800

    fix: 插播权限

--- src/packages/aside-menu/src/main.vue
+++ src/packages/aside-menu/src/main.vue
@@ -314,7 +314,7 @@
             } else {
               if (this.isInGroup) {
                 vn.setHiddenState(true);
-              } else if (this.presenterId != this.doc_permission) {
+              } else if (this.presenterId != this.doc_permission || this.isThirdStream) {
                 // 主讲人和演示人，不是同一个人，说面有人在演示，就禁用助理的插播
                 vn.setHiddenState(false);
                 vn.setDisableState(true);
