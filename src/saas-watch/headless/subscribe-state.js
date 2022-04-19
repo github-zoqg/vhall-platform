09f257c85260618bf2d8440d535dc7f67afec59f
qingzhao.xu <qingzhao.xu@vhall.com>
2022-04-19 16:25:14 +0800

    fix: lint

--- src/saas-watch/headless/subscribe-state.js
+++ src/saas-watch/headless/subscribe-state.js
@@ -1,6 +1,6 @@
 import { useMsgServer, useRoomBaseServer, useUserServer } from 'middle-domain';
 
-export default async function() {
+export default async function () {
   console.log('%c------服务初始化 开始', 'color:blue');
   const roomBaseServer = useRoomBaseServer();
   const msgServer = useMsgServer();
