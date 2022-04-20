3360f249d2fb01fee6108df034c48893bd667470
xiaodong.ding <xiaodong.ding@vhall.com>
2022-04-20 15:04:13 +0800

    chore: 更新版本号6.5.0-3

--- src/saas-wap/webpack.prod.config.js
+++ src/saas-wap/webpack.prod.config.js
@@ -5,7 +5,7 @@ const htmlConfig = {
   cdnJs: {
     MiddleEventSdk: '//cnstatic01.e.vhall.com/common-static/middle/middle-event-sdk/0.3.1/index.js',
     MiddleDomain:
-      '//cnstatic01.e.vhall.com/common-static/middle/middle-domain/6.5.0-2/middle-domain.js'
+      '//cnstatic01.e.vhall.com/common-static/middle/middle-domain/6.5.0-3/middle-domain.js'
   },
   // dns-prefetch
   dnsPrefetch: {
