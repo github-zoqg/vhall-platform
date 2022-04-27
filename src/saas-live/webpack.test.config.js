275e81d9d991505dfb1d3c1eda6f5904b0b45be4
zhangxiao <215906340>
2022-04-27 15:52:52 +0800

    feat: 修改版本号

--- src/saas-live/webpack.test.config.js
+++ src/saas-live/webpack.test.config.js
@@ -7,7 +7,7 @@ const htmlConfig = {
     MiddleEventSdk:
       '//t-alistatic01.e.vhall.com/common-static/middle/middle-event-sdk/0.3.1/index.js',
     MiddleDomain:
-      '//t-alistatic01.e.vhall.com/common-static/middle/middle-domain/6.5.0-13/middle-domain.js'
+      '//t-alistatic01.e.vhall.com/common-static/middle/middle-domain/6.5.0-14/middle-domain.js'
   }
 };
 
