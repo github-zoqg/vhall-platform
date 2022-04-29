1ee8095781e866243dfa4a8f610f1ac3d76f11a5
xushuo <893804934@qq.com>
2022-04-29 15:43:11 +0800

    Merge remote-tracking branch 'upstream/fix#chat_wap_input' into fix_chat
    # Conflicts:
    #	src/packages/chat-wap/src/main.vue

--- src/saas-wap/webpack.base.config.js
+++ src/saas-wap/webpack.base.config.js
@@ -11,12 +11,13 @@ const htmlConfig = {
   cdnJs: {
     lodash: '//s1.e.vhall.com/common-static/middle/lodash/4.17.21/lodash.min.js',
     vue: '//cnstatic01.e.vhall.com/common-static/middle/vue/2.6.14/dist/vue.min.js',
-    VueRouter: '//cnstatic01.e.vhall.com/common-static/middle/vue-router/3.5.3/dist/vue-router.min.js',
+    VueRouter:
+      '//cnstatic01.e.vhall.com/common-static/middle/vue-router/3.5.3/dist/vue-router.min.js',
     Moment: '//cnstatic01.e.vhall.com/common-static/middle/moment/2.29.1/dist/moment.min.js',
     jsCookie: '//cnstatic01.e.vhall.com/common-static/middle/js-cookie/3.0.1/js.cookie.min.js',
     VueI18n: '//cnstatic01.e.vhall.com/common-static/middle/vue-i18n/8.26.7/vue-i18n.min.js',
     MiddleEventSdk: '//cnstatic01.e.vhall.com/common-static/middle/middle-event-sdk/0.3.1/index.js',
-    MiddleDomain: 'http://172.16.26.184:10001/lib/middle-domain.js',
+    MiddleDomain: 'http://localhost:10001/lib/middle-domain.js',
 
     Vant: '//s1.e.vhall.com/common-static/middle/vant/2.12.34/dist/vant.min.js',
     // TODO: 图片验证码涉及到的地方，需引入该文件，如何放置
@@ -34,13 +35,9 @@ const htmlConfig = {
   cdnCss: {
     ElementUi: '//s2.e.vhall.com/common-static/middle/element-ui/lib/2.6.2/theme-chalk/index.css',
     Vant: '//s2.e.vhall.com/common-static/middle/vant/2.12.34/dist/index.css',
-    // 开发时暂时使用阿里链接，后面改成自己的cdn
-    iconfontCommon: '//cnstatic01.e.vhall.com/common-static/middle/iconfont/vh-saas/v1.0.2/iconfont.css',
+    iconfontCommon:
+      '//cnstatic01.e.vhall.com/common-static/middle/iconfont/vh-saas/v1.0.2/iconfont.css',
     iconfont: '//cnstatic01.e.vhall.com/common-static/middle/iconfont/vh-pc/v1.0.2/iconfont.css'
-  },
-  // dns-prefetch
-  dnsPrefetch: {
-    open: false
   }
 };
 
