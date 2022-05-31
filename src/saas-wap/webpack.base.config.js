3cc7289a958bb2a09e0939fea05278e5771f10bc
yongkai.shao <yongkai.shao@vhall.com>
2022-05-31 22:22:08 +0800

    perf: 修改默认第三方依赖资源加载域名分担并发请求

--- src/saas-wap/webpack.base.config.js
+++ src/saas-wap/webpack.base.config.js
@@ -9,24 +9,29 @@ const pkg = require('./package.json');
 const htmlConfig = {
   // cdn js
   cdnJs: {
+    // 第三方依赖
     lodash: '//s1.e.vhall.com/common-static/middle/lodash/4.17.21/lodash.min.js',
-    vue: '//cnstatic01.e.vhall.com/common-static/middle/vue/2.6.14/dist/vue.min.js',
-    VueRouter:
-      '//cnstatic01.e.vhall.com/common-static/middle/vue-router/3.5.3/dist/vue-router.min.js',
-    Moment: '//cnstatic01.e.vhall.com/common-static/middle/moment/2.29.1/dist/moment.min.js',
-    jsCookie: '//cnstatic01.e.vhall.com/common-static/middle/js-cookie/3.0.1/js.cookie.min.js',
-    VueI18n: '//cnstatic01.e.vhall.com/common-static/middle/vue-i18n/8.26.7/vue-i18n.min.js',
-    MiddleEventSdk: '//cnstatic01.e.vhall.com/common-static/middle/middle-event-sdk/0.3.1/index.js',
-    MiddleDomain: 'http://localhost:10001/lib/middle-domain.js',
+    vue: '//s1.e.vhall.com/common-static/middle/vue/2.6.14/dist/vue.min.js',
+    VueRouter: '//s1.e.vhall.com/common-static/middle/vue-router/3.5.3/dist/vue-router.min.js',
+    Moment: '//s1.e.vhall.com/common-static/middle/moment/2.29.1/dist/moment.min.js',
+    jsCookie: '//s1.e.vhall.com/common-static/middle/js-cookie/3.0.1/js.cookie.min.js',
+    VueI18n: '//s1.e.vhall.com/common-static/middle/vue-i18n/8.26.7/vue-i18n.min.js',
 
-    Vant: '//s1.e.vhall.com/common-static/middle/vant/2.12.34/dist/vant.min.js',
+    Vant: '//cnstatic01.e.vhall.com/common-static/middle/vant/2.12.34/dist/vant.min.js',
     // TODO: 图片验证码涉及到的地方，需引入该文件，如何放置
-    imageYunDun: '//cstaticdun.126.net/load.min.js?t=201903281201',
-    jsencrypt: '//s1.e.vhall.com/common-static/middle/jsencrypt/3.2.1/dist/jsencrypt.min.js',
-    loadJs: '//s1.e.vhall.com/common-static/middle/loadjs/4.2.0/loadjs.min.js',
-    wx: '//s1.e.vhall.com/common-static/middle/loadjweixin/1.0.0/index.min.js',
-    iOSUserScalable: '//s1.e.vhall.com/common-static/middle/iOSUserScalable/1.0.0/index.min.js',
-    ElementUi: '//s1.e.vhall.com/common-static/middle/element-ui/lib/2.6.2/index.js',
+    imageYunDun: '//cnstatic01.e.vhall.com/load.min.js?t=201903281201',
+    jsencrypt:
+      '//cnstatic01.e.vhall.com/common-static/middle/jsencrypt/3.2.1/dist/jsencrypt.min.js',
+    loadJs: '//cnstatic01.e.vhall.com/common-static/middle/loadjs/4.2.0/loadjs.min.js',
+    wx: '//cnstatic01.e.vhall.com/common-static/middle/loadjweixin/1.0.0/index.min.js',
+    iOSUserScalable:
+      '//cnstatic01.e.vhall.com/common-static/middle/iOSUserScalable/1.0.0/index.min.js',
+
+    ElementUi: '//cnstatic01.e.vhall.com/common-static/middle/element-ui/lib/2.6.2/index.js',
+
+    // 自开发依赖
+    MiddleEventSdk: '//s2.e.vhall.com/common-static/middle/middle-event-sdk/0.3.1/index.js',
+    MiddleDomain: 'http://localhost:10001/lib/middle-domain.js',
 
     // 移动端网页调试神器
     Erdua: '//t-alistatic01.e.vhall.com/common-static/middle/eruda/2.4.1/eruda.js'
@@ -35,9 +40,8 @@ const htmlConfig = {
   cdnCss: {
     ElementUi: '//s2.e.vhall.com/common-static/middle/element-ui/lib/2.6.2/theme-chalk/index.css',
     Vant: '//s2.e.vhall.com/common-static/middle/vant/2.12.34/dist/index.css',
-    iconfontCommon:
-      '//cnstatic01.e.vhall.com/common-static/middle/iconfont/vh-saas/v1.0.4/iconfont.css',
-    iconfont: '//cnstatic01.e.vhall.com/common-static/middle/iconfont/vh-pc/v1.0.4/iconfont.css'
+    iconfontCommon: '//s2.e.vhall.com/common-static/middle/iconfont/vh-saas/v1.0.4/iconfont.css',
+    iconfont: '//s2.e.vhall.com/common-static/middle/iconfont/vh-pc/v1.0.4/iconfont.css'
   }
 };
 
