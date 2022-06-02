be023a7ab03726a7bfb2c87c964a698c97828ac8
yongkai.shao <yongkai.shao@vhall.com>
2022-06-02 15:56:28 +0800

    feat: 修改加载资源

--- src/saas-wap/webpack.base.config.js
+++ src/saas-wap/webpack.base.config.js
@@ -17,24 +17,25 @@ const htmlConfig = {
     jsCookie: '//s1.e.vhall.com/common-static/middle/js-cookie/3.0.1/js.cookie.min.js',
     VueI18n: '//s1.e.vhall.com/common-static/middle/vue-i18n/8.26.7/vue-i18n.min.js',
 
-    Vant: '//cnstatic01.e.vhall.com/common-static/middle/vant/2.12.34/dist/vant.min.js',
+    // 通过s3域名加载
+    Vant: '//s3.e.vhall.com/common-static/middle/vant/2.12.34/dist/vant.min.js',
+    loadJs: '//s3.e.vhall.com/common-static/middle/loadjs/4.2.0/loadjs.min.js',
+
+    loadLazyWap: '//s3.e.vhall.com/common-static/middle/loadLazyWap/1.1.0/index.min.js',
+    // iOSUserScalable: '//s3.e.vhall.com/common-static/middle/iOSUserScalable/1.1.0/index.min.js',
+    ElementUi: '//s3.e.vhall.com/common-static/middle/element-ui/lib/2.6.2/index.js',
+
+    // 通过s4域名加载, 移动端网页调试神器
+    Erdua: '//s4.e.vhall.com/common-static/middle/eruda/2.4.1/eruda.js',
+
+    // 可以延迟加载的
     // TODO: 图片验证码涉及到的地方，需引入该文件，如何放置
     imageYunDun: '//cstaticdun.126.net/load.min.js?t=201903281201',
-    jsencrypt:
-      '//cnstatic01.e.vhall.com/common-static/middle/jsencrypt/3.2.1/dist/jsencrypt.min.js',
-    loadJs: '//cnstatic01.e.vhall.com/common-static/middle/loadjs/4.2.0/loadjs.min.js',
-    wx: '//cnstatic01.e.vhall.com/common-static/middle/loadjweixin/1.0.0/index.min.js',
-    iOSUserScalable:
-      '//cnstatic01.e.vhall.com/common-static/middle/iOSUserScalable/1.0.0/index.min.js',
-
-    ElementUi: '//cnstatic01.e.vhall.com/common-static/middle/element-ui/lib/2.6.2/index.js',
+    jsencrypt: '//s3.e.vhall.com/common-static/middle/jsencrypt/3.2.1/dist/jsencrypt.min.js',
 
     // 自开发依赖
     MiddleEventSdk: '//s2.e.vhall.com/common-static/middle/middle-event-sdk/0.3.1/index.js',
-    MiddleDomain: 'http://localhost:10001/lib/middle-domain.js',
-
-    // 移动端网页调试神器
-    Erdua: '//t-alistatic01.e.vhall.com/common-static/middle/eruda/2.4.1/eruda.js'
+    MiddleDomain: 'http://localhost:10001/lib/middle-domain.js'
   },
   // cdn css
   cdnCss: {
