91b248b82981ec68cca8b6598a9498c2cea99e64
xushuo <893804934@qq.com>
2022-06-08 12:05:58 +0800

    fix: 多语言代码添加以及旧版本礼物适配

--- src/packages/chat-wap/src/components/msg-item.vue
+++ src/packages/chat-wap/src/components/msg-item.vue
@@ -65,7 +65,7 @@
             <span>{{ source.roleName | roleFilter }}</span>
           </span>
           {{ source.content.text_content }}
-          <span class="highlight">{{ $t('chat.chat_1029') }}</span>
+          <span class="highlight">{{ $t('chat.chat_1093') }}</span>
         </div>
       </div>
     </template>
