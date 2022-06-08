aace1836c02563555ad3855d20dd1cfb8972588f
xushuo <893804934@qq.com>
2022-06-08 10:34:50 +0800

    fix: 动效SVGA文件资源地址动态添加

--- src/packages/pc-reward-effect-full-screen/src/source/index.js
+++ src/packages/pc-reward-effect-full-screen/src/source/index.js
@@ -1,12 +1,12 @@
 const sources = {
-  666: 'https://t-alistatic01.e.vhall.com/common-static/svga/1.0.0/666.svga',
-  玫瑰: 'https://t-alistatic01.e.vhall.com/common-static/svga/1.0.0/flower.svga',
-  咖啡: 'https://t-alistatic01.e.vhall.com/common-static/svga/1.0.0/coffee.svga',
-  爱心: 'https://t-alistatic01.e.vhall.com/common-static/svga/1.0.0/love.svga',
-  飞机: 'https://t-alistatic01.e.vhall.com/common-static/svga/1.0.0/plane.svga',
-  火箭: 'https://t-alistatic01.e.vhall.com/common-static/svga/1.0.0/rocket.svga',
-  汽车: 'https://t-alistatic01.e.vhall.com/common-static/svga/1.0.0/car.svga',
-  礼炮: 'https://t-alistatic01.e.vhall.com/common-static/svga/1.0.0/fireworks.svga',
-  打赏: 'https://t-alistatic01.e.vhall.com/common-static/svga/1.0.0/wallet.svga'
+  666: process.env.VUE_APP_STATIC_BASE + '/common-static/svga/1.0.0/666.svga',
+  玫瑰: process.env.VUE_APP_STATIC_BASE + '/common-static/svga/1.0.0/flower.svga',
+  咖啡: process.env.VUE_APP_STATIC_BASE + '/common-static/svga/1.0.0/coffee.svga',
+  爱心: process.env.VUE_APP_STATIC_BASE + '/common-static/svga/1.0.0/love.svga',
+  飞机: process.env.VUE_APP_STATIC_BASE + '/common-static/svga/1.0.0/plane.svga',
+  火箭: process.env.VUE_APP_STATIC_BASE + '/common-static/svga/1.0.0/rocket.svga',
+  汽车: process.env.VUE_APP_STATIC_BASE + '/common-static/svga/1.0.0/car.svga',
+  礼炮: process.env.VUE_APP_STATIC_BASE + '/common-static/svga/1.0.0/fireworks.svga',
+  打赏: process.env.VUE_APP_STATIC_BASE + '/common-static/svga/1.0.0/wallet.svga'
 };
 export default sources;
