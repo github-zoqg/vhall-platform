e1d913750ec6ecb500df00cf1c2f943150f8d6d3
xinyuan.yang <xinyuan.yang@vhall.com>
2022-06-10 14:37:56 +0800

    chore: iconfont.cssy引用压缩版

--- src/saas-wap/webpack.base.config.js
+++ src/saas-wap/webpack.base.config.js
@@ -34,8 +34,8 @@ const htmlConfig = {
     ElementUi: '//s2.e.vhall.com/common-static/middle/element-ui/lib/2.6.2/theme-chalk/index.css',
     Vant: '//s2.e.vhall.com/common-static/middle/vant/2.12.34/dist/index.css',
     iconfontCommon:
-      '//cnstatic01.e.vhall.com/common-static/middle/iconfont/vh-saas/v1.0.5/iconfont.css',
-    iconfont: '//cnstatic01.e.vhall.com/common-static/middle/iconfont/vh-pc/v1.0.5/iconfont.css'
+      '//cnstatic01.e.vhall.com/common-static/middle/iconfont/vh-saas/v1.0.5/iconfont.min.css',
+    iconfont: '//cnstatic01.e.vhall.com/common-static/middle/iconfont/vh-pc/v1.0.5/iconfont.min.css'
   }
 };
 
--- src/saas-watch/webpack.base.config.js
+++ src/saas-watch/webpack.base.config.js
@@ -29,8 +29,8 @@ const htmlConfig = {
     ElementUi:
       '//cnstatic01.e.vhall.com/common-static/middle/element-ui/lib/2.6.2/theme-chalk/index.css',
     iconfontCommon:
-      '//cnstatic01.e.vhall.com/common-static/middle/iconfont/vh-saas/v1.0.5/iconfont.css',
-    iconfont: '//cnstatic01.e.vhall.com/common-static/middle/iconfont/vh-pc/v1.0.5/iconfont.css'
+      '//cnstatic01.e.vhall.com/common-static/middle/iconfont/vh-saas/v1.0.5/iconfont.min.css',
+    iconfont: '//cnstatic01.e.vhall.com/common-static/middle/iconfont/vh-pc/v1.0.5/iconfont.min.css'
   }
 };
 
