ca03645c227bae3a0703c49b32003e049de60c09
xinyuan.yang <xinyuan.yang@vhall.com>
2022-06-15 15:28:40 +0800

    chore: vh5-ui引用改成cdn方式

--- src/saas-wap/webpack.base.config.js
+++ src/saas-wap/webpack.base.config.js
@@ -18,7 +18,7 @@ const htmlConfig = {
     ElementUi: '//s1.e.vhall.com/common-static/middle/element-ui/lib/2.6.2/index.js',
 
     // 通过s3域名加载
-    Vant: '//s3.e.vhall.com/common-static/middle/vant/2.12.34/dist/vant.min.js',
+    Vh5Ui: '//s3.e.vhall.com/common-static/middle/vh5-ui/1.0.4/js/vh5-ui.min.js',
     loadLazyWap: '//s3.e.vhall.com/common-static/middle/loadLazyWap/1.1.0/index.min.js',
 
     // 可以延迟加载的
@@ -32,7 +32,8 @@ const htmlConfig = {
   // cdn css
   cdnCss: {
     ElementUi: '//s2.e.vhall.com/common-static/middle/element-ui/lib/2.6.2/theme-chalk/index.css',
-    Vant: '//s2.e.vhall.com/common-static/middle/vant/2.12.34/dist/index.css',
+    Vh5Ui: '//s3.e.vhall.com/common-static/middle/vh5-ui/1.0.4/css/vh5-ui.min.css',
+
     iconfontCommon:
       '//cnstatic01.e.vhall.com/common-static/middle/iconfont/vh-saas/v1.0.5/iconfont.min.css',
     iconfont: '//cnstatic01.e.vhall.com/common-static/middle/iconfont/vh-pc/v1.0.5/iconfont.min.css'
