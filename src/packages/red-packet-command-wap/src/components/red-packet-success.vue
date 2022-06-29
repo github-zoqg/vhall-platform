ba89861b46645790041b5d8ebebbdeb11dc5e720
xushuo <893804934@qq.com>
2022-06-29 20:03:24 +0800

    fix: 复制功能ios兼容

--- src/packages/red-packet-command-wap/src/components/red-packet-success.vue
+++ src/packages/red-packet-command-wap/src/components/red-packet-success.vue
@@ -16,7 +16,6 @@
     </div>
     <!-- 第四层 内容层-->
     <div class="vhsaas-red-packet-content">
-      <input type="text" ref="shareLink" class="copyText" readonly v-model="red_code" />
       <div class="btn btn-copy" v-if="!!red_code" @click="copy">
         {{ $t('interact_tools.interact_tools_1082') }}
       </div>
@@ -54,9 +53,49 @@
         this.$emit('navTo', 'RedPacketWinners');
       },
       copy() {
-        this.$refs.shareLink.select(); // 选择对象
-        document.execCommand('Copy'); // 执行浏览器复制命令
-        this.$toast(this.$t('nav.nav_1024'));
+        // this.$refs.shareLink.select(); // 选择对象
+        // document.execCommand('Copy'); // 执行浏览器复制命令
+
+        const textString = this.red_code;
+        let input = document.querySelector('#copy-input');
+        if (!input) {
+          input = document.createElement('input');
+          input.id = 'copy-input';
+          input.readOnly = 'readOnly'; // 防止ios聚焦触发键盘事件
+          input.style.position = 'absolute';
+          input.style.left = '-1000px';
+          input.style.zIndex = '-1000';
+          input.style.border = 'none';
+          document.body.appendChild(input);
+        }
+
+        input.value = textString;
+        // ios必须先选中文字且不支持 input.select();
+        selectText(input, 0, textString.length);
+        if (document.execCommand('copy')) {
+          document.execCommand('copy');
+          this.$toast(this.$t('nav.nav_1024'));
+        } else {
+          console.log('不兼容');
+        }
+        input.blur();
+
+        // input自带的select()方法在苹果端无法进行选择，所以需要自己去写一个类似的方法
+        // 选择文本。createTextRange(setSelectionRange)是input方法
+        function selectText(textbox, startIndex, stopIndex) {
+          if (textbox.createTextRange) {
+            //ie
+            const range = textbox.createTextRange();
+            range.collapse(true);
+            range.moveStart('character', startIndex); //起始光标
+            range.moveEnd('character', stopIndex - startIndex); //结束光标
+            range.select(); //不兼容苹果
+          } else {
+            //firefox/chrome
+            textbox.setSelectionRange(startIndex, stopIndex);
+            textbox.focus();
+          }
+        }
       }
     }
   };
