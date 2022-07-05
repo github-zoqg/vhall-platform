d5dc7b475a1fb746d1775ec0e0de8b1291c56303
xiao.zhang <xiao.zhang@vhall.com>
2022-07-05 19:23:49 +0800

    feat: 修改

--- src/packages/wap-subscribe-body/src/main.vue
+++ src/packages/wap-subscribe-body/src/main.vue
@@ -497,6 +497,9 @@
               if (location.pathname.indexOf('embedclient') != -1) {
                 pageUrl = '/embedclient';
               }
+              // 如果往观看页跳转，需要清除暖场视频缓存
+              window.sessionStorage.removeItem('warm_recordId');
+              window.sessionStorage.removeItem('recordIds');
               location.replace(
                 window.location.origin +
                   process.env.VUE_APP_ROUTER_BASE_URL +
