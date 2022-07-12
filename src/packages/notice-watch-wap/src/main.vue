d3c096382dedcb3ea3790891a9b5ff4a7cefeee6
xiaodong.ding <xiaodong.ding@vhall.com>
2022-07-12 17:42:53 +0800

    feat: 切换视频播放器位置对应公告处理

--- src/packages/notice-watch-wap/src/main.vue
+++ src/packages/notice-watch-wap/src/main.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="vmp-notice-wap">
+  <div class="vmp-notice-wap" :class="wapNoticeClass">
     <van-notice-bar
       class="notice"
       ref="notice"
@@ -24,17 +24,69 @@
           content: '12313',
           isShow: false,
           timer: null
-        }
+        },
+        mini: false
       };
     },
     computed: {
       isInGroup() {
         return this.$domainStore.state.groupServer.groupInitData.isInGroup;
+      },
+      isFixedIndex() {
+        return this.$domainStore.state.menuServer.selectedType == 2 && this.isWapBodyDocSwitch;
+      },
+      isSmallPlayer() {
+        return this.$domainStore.state.playerServer.isSmallPlayer;
+      },
+      // wap-body和文档是否切换位置
+      isWapBodyDocSwitch() {
+        return this.$domainStore.state.roomBaseServer.isWapBodyDocSwitch;
+      },
+      wapNoticeClass() {
+        let className = '';
+        // showHeader: 是否展示顶部header组件，控制台配置
+        // isWapBodyDocSwitch: wapBody 和 文档组件是否切换位置
+        // isSmallPlayer: 音频播放器是否缩小
+        // mini: 播放器是否是mini状态（打开问卷）
+        if (this.mini || !this.isFixedIndex) return '';
+        if (this.showHeader && this.isWapBodyDocSwitch) {
+          className = 'vmp-notice-wap__bottom';
+          if (this.isSmallPlayer) {
+            className += ' vmp-notice-wap__bottom__small';
+          }
+          return className;
+        }
+        if (!this.showHeader && this.isWapBodyDocSwitch) {
+          className = 'vmp-notice-wap__bottom-noheader';
+          if (this.isSmallPlayer) {
+            className += ' vmp-notice-wap__bottom-noheader__small';
+          }
+          return className;
+        }
+        return '';
+      },
+      /**
+       * 是否显示头部
+       */
+      showHeader() {
+        if (this.embedObj.embed || (this.webinarTag && this.webinarTag.organizers_status == 0)) {
+          return false;
+        } else {
+          return true;
+        }
+      },
+      // 是否为嵌入页
+      embedObj() {
+        return this.$domainStore.state.roomBaseServer.embedObj;
+      },
+      // 主办方配置
+      webinarTag() {
+        return this.$domainStore.state.roomBaseServer.webinarTag;
       }
     },
     watch: {
       isInGroup(val) {
-        this.announcement.isShow = false;
+        this.announcement.isShow = true;
         this.openAnnouncement();
       }
     },
@@ -56,28 +108,31 @@
           clearTimeout(this.timer);
         }
         this.timer = setTimeout(() => {
-          this.announcement.isShow = false;
+          this.announcement.isShow = true;
         }, 30000);
       });
       // 直播结束自动关闭公告
       this.noticeServer.$on('live_over', () => {
-        this.announcement.isShow = false;
+        this.announcement.isShow = true;
       });
       // 结束讨论
       this.groupServer.$on('ROOM_CHANNEL_CHANGE', () => {
         if (!this.isInGroup) {
           this.openAnnouncement();
         } else {
-          this.announcement.isShow = false;
+          this.announcement.isShow = true;
         }
       });
       this.groupServer.$on('GROUP_SWITCH_START', () => {
-        this.announcement.isShow = false;
+        this.announcement.isShow = true;
       });
     },
     methods: {
+      questionnaireVisible(flag) {
+        this.mini = flag;
+      },
       closeAnnouncement() {
-        this.announcement.isShow = false;
+        this.announcement.isShow = true;
       },
       openAnnouncement() {
         // 刷新展示最新一条公告&&为直播状态
@@ -94,10 +149,10 @@
             clearTimeout(this.timer);
           }
           this.timer = setTimeout(() => {
-            this.announcement.isShow = false;
+            this.announcement.isShow = true;
           }, 30000);
         } else {
-          this.announcement.isShow = false;
+          this.announcement.isShow = true;
         }
       }
     }
@@ -114,6 +169,26 @@
     }
     .van-notice-bar {
       box-sizing: border-box;
+    }
+    &__bottom {
+      position: fixed;
+      left: 0;
+      top: 573px;
+      width: 100%;
+      z-index: 2;
+      .notice {
+        top: 0;
+      }
+    }
+    &__bottom-noheader {
+      position: fixed;
+      left: 0;
+      top: 502px;
+      width: 100%;
+      z-index: 2;
+      .notice {
+        top: 0;
+      }
     }
   }
 </style>
