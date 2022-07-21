4cbefa1ba457e0f23d7e39b417ae47133fa90510
xushuo <893804934@qq.com>
2022-07-21 14:35:52 +0800

    fix: 正常消息样式修改以及逻辑补充

--- src/packages/concise-style/chat-wap/src/components/msg-item.vue
+++ src/packages/concise-style/chat-wap/src/components/msg-item.vue
@@ -98,8 +98,9 @@
       <div v-if="source.content.gift_name" class="msg-item new-gift">
         <div class="interact-gift-box" :class="source.content.source_status == 1 ? 'zdy' : ''">
           <span class="new-gift-name">
-            {{ source.nickname | overHidden(8) }}
+            {{ source.nickname | overHidden(6) }}
           </span>
+          &nbsp;
           <span class="new-gift-content">
             {{ $t('chat.chat_1061') }} {{ source.content.gift_name | overHidden(10) }}
           </span>
@@ -183,25 +184,31 @@
           <!-- @消息 -->
           <template v-if="source.atList && source.atList.length !== 0">
             <div class="msg-content_body">
-              <span
-                v-if="source.roleName && source.roleName != '2'"
-                class="role"
-                :class="source.roleName | roleClassFilter"
-              >
-                {{ source.roleName | roleFilter }}
-              </span>
-              <span class="nickname">{{ source.nickname | overHidden(6) }}：</span>
-              <span v-html="msgContent" class="chat-text"></span>
-              <div
-                @click="previewImg(img, index, source.content.image_urls)"
-                class="msg-content_chat-img"
-                v-for="(img, index) in source.content.image_urls"
-                :key="index"
-                :style="`backgroundImage: url('${
-                  img + '?x-oss-process=image/resize,m_lfit,h_60,w_60'
-                }')`"
-                :alt="$t('chat.chat_1065')"
-              ></div>
+              <div class="normal-msg">
+                <div class="textInfo">
+                  <span
+                    v-if="source.roleName && source.roleName != '2'"
+                    class="role"
+                    :class="source.roleName | roleClassFilter"
+                  >
+                    {{ source.roleName | roleFilter }}
+                  </span>
+                  <span class="nickname">{{ source.nickname | overHidden(6) }}：</span>
+                  <span v-html="msgContent" class="chat-text"></span>
+                </div>
+                <div class="imgs">
+                  <div
+                    @click="previewImg(img, index, source.content.image_urls)"
+                    class="img"
+                    v-for="(img, index) in source.content.image_urls"
+                    :key="index"
+                    :style="`backgroundImage: url('${
+                      img + '?x-oss-process=image/resize,m_lfit,h_60,w_60'
+                    }')`"
+                    :alt="$t('chat.chat_1065')"
+                  ></div>
+                </div>
+              </div>
             </div>
           </template>
           <!-- 正常消息 -->
@@ -212,25 +219,39 @@
             "
           >
             <div class="msg-content_body">
-              <div
-                class="textInfo"
-                :class="
-                  !!!msgContent && source.content.image_urls.length != 0 ? 'existSimpleImg' : ''
-                "
-              >
-                <span
-                  v-if="source.roleName && source.roleName != '2'"
-                  class="role"
-                  :class="source.roleName | roleClassFilter"
+              <div class="normal-msg">
+                <div
+                  class="textInfo"
+                  :class="
+                    !!!msgContent && source.content.image_urls.length != 0 ? 'existSimpleImg' : ''
+                  "
                 >
-                  <span>{{ source.roleName | roleFilter }}</span>
-                </span>
-                <span class="nickname">{{ source.nickname | overHidden(6) }}：</span>
-                <span v-html="msgContent" class="chat-text"></span>
-                <template v-if="!!!msgContent">
+                  <span
+                    v-if="source.roleName && source.roleName != '2'"
+                    class="role"
+                    :class="source.roleName | roleClassFilter"
+                  >
+                    <span>{{ source.roleName | roleFilter }}</span>
+                  </span>
+                  <span class="nickname">{{ source.nickname | overHidden(6) }}：</span>
+                  <span v-html="msgContent" class="chat-text"></span>
+                  <template v-if="!!!msgContent">
+                    <div
+                      @click="previewImg(img, index, source.content.image_urls)"
+                      class="msg-content_chat-img"
+                      v-for="(img, index) in source.content.image_urls"
+                      :key="index"
+                      :style="`backgroundImage: url('${
+                        img + '?x-oss-process=image/resize,m_lfit,h_60,w_60'
+                      }')`"
+                      :alt="$t('chat.chat_1065')"
+                    ></div>
+                  </template>
+                </div>
+                <div v-if="!!msgContent" class="imgs">
                   <div
                     @click="previewImg(img, index, source.content.image_urls)"
-                    class="msg-content_chat-img"
+                    class="img"
                     v-for="(img, index) in source.content.image_urls"
                     :key="index"
                     :style="`backgroundImage: url('${
@@ -238,20 +259,8 @@
                     }')`"
                     :alt="$t('chat.chat_1065')"
                   ></div>
-                </template>
+                </div>
               </div>
-              <template v-if="!!msgContent">
-                <div
-                  @click="previewImg(img, index, source.content.image_urls)"
-                  class="msg-content_chat-img"
-                  v-for="(img, index) in source.content.image_urls"
-                  :key="index"
-                  :style="`backgroundImage: url('${
-                    img + '?x-oss-process=image/resize,m_lfit,h_60,w_60'
-                  }')`"
-                  :alt="$t('chat.chat_1065')"
-                ></div>
-              </template>
             </div>
           </template>
         </div>
@@ -359,23 +368,23 @@
         //@用户
         //todo 可以考虑domaint提供统一的处理 实现@用户
         this.msgContent = this.urlToLink(this.source.content.text_content);
-        this.source.atList &&
-          this.source.atList.forEach(a => {
-            // TODO历史列表aList与直播中格式不一致作
-            const userName = `@${a.nick_name || a.nickName} `;
-            const match =
-              this.source.content &&
-              this.source.content.text_content &&
-              this.source.content.text_content.indexOf(userName) != -1;
-            if (match) {
-              this.msgContent = this.urlToLink(
-                this.source.content.text_content.replace(
-                  userName,
-                  `<span style='color:#3562fa'>${userName}</span>`
-                )
-              );
-            }
-          });
+        // this.source.atList &&
+        //   this.source.atList.forEach(a => {
+        //     // TODO历史列表aList与直播中格式不一致作
+        //     const userName = `@${a.nick_name || a.nickName} `;
+        //     const match =
+        //       this.source.content &&
+        //       this.source.content.text_content &&
+        //       this.source.content.text_content.indexOf(userName) != -1;
+        //     if (match) {
+        //       this.msgContent = this.urlToLink(
+        //         this.source.content.text_content.replace(
+        //           userName,
+        //           `<span style='color:#3562fa'>${userName}</span>`
+        //         )
+        //       );
+        //     }
+        //   });
         if (
           (this.source.atList || []).find(u => this.joinInfo.third_party_user_id == u.accountId) &&
           !this.source.isHistoryMsg
@@ -477,39 +486,42 @@
               background-color: #0a7ff5;
             }
           }
-          > .textInfo {
-            line-height: 1.46;
-            &.existSimpleImg {
-              padding-top: 5px;
-            }
-            span {
-              word-break: break-word;
-            }
-            .chat-text {
-              img {
-                width: 34px;
-                height: 34px;
+          > .normal-msg {
+            > .textInfo {
+              line-height: 1.46;
+              &.existSimpleImg {
+                padding-top: 5px;
+              }
+              span {
+                word-break: break-word;
+              }
+              .chat-text {
+                img {
+                  width: 34px;
+                  height: 34px;
+                }
+              }
+              .msg-content_chat-img {
+                vertical-align: text-top;
+                margin-top: 0;
               }
             }
-            .msg-content_chat-img {
-              vertical-align: text-top;
-              margin-top: 0;
+            > .imgs {
+              .img {
+                display: inline-block;
+                margin: 8px 8px 5px 0;
+                width: 60px;
+                height: 60px;
+                border-radius: 4px;
+                background-size: cover;
+                background-repeat: no-repeat;
+                background-position: center;
+                &:nth-last-child(1) {
+                  margin-right: 0;
+                }
+              }
             }
           }
-          > .msg-content_chat-img {
-            display: inline-block;
-            margin: 8px 8px 5px 0;
-            width: 60px;
-            height: 60px;
-            border-radius: 4px;
-            background-size: cover;
-            background-repeat: no-repeat;
-            background-position: center;
-            &:nth-last-child(1) {
-              margin-right: 0;
-            }
-          }
-
           > .reply-msg {
             padding-left: 18px;
             margin-bottom: 8px;
@@ -577,30 +589,79 @@
       }
       &.new-gift,
       &.interact {
-        margin: 0 auto;
         width: fit-content;
-        > div {
-          background: rgba(255, 209, 201, 0.2);
-          border-radius: 40px;
+        padding-left: 0;
+        position: relative;
+        display: block;
+        color: #fff;
+        &.interact {
+          justify-content: unset;
         }
-        .role {
-          border-radius: 16px;
-          padding: 0 6px;
-          margin-right: 8px;
-          font-size: 22px;
-          line-height: 22px;
-          &.host {
-            background: rgba(251, 38, 38, 0.1);
-            color: #fb2626;
+        &.purpose {
+          background: linear-gradient(221deg, rgba(184, 58, 244, 0) 0%, #6950fb 100%);
+        }
+        &.red {
+          background: linear-gradient(227deg, rgba(255, 137, 96, 0) 0%, #ff6267 100%);
+        }
+        &.reward_pay_ok {
+          .interact-gift-box {
+            height: 88px;
+            padding: 0 74px 0 24px;
+            .reward_txt {
+              line-height: 1;
+              margin-top: 4px;
+            }
           }
-          &.assistant {
-            background-color: rgba(173, 225, 255, 0.5);
-            color: #0a7ff5;
+        }
+        > .interact-gift-box {
+          position: relative;
+          background: rgba(0, 0, 0, 0.25);
+          border-radius: 20px;
+          padding: 0 60px 0 16px;
+          text-align: left;
+          width: 100%;
+          height: 46px;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          border: none;
+          font-size: 26px;
+          &.zdy {
+            padding-right: 68px;
           }
-          &.guest {
-            background-color: rgba(173, 225, 255, 0.5);
-            color: #0a7ff5;
+          .flex-box {
+            display: flex;
+            justify-content: center;
+            align-items: center;
           }
+          .gift-zdy {
+            width: 36px;
+            height: 36px;
+            border-radius: 50%;
+            position: absolute;
+            right: 24px;
+            background-color: white;
+            background-size: contain;
+            background-repeat: no-repeat;
+            background-position: center;
+          }
+          .new-gift-img {
+            width: 36px;
+            position: absolute;
+            right: 16px;
+          }
+        }
+        .new-gift-img,
+        .new-award-img {
+          width: 60px;
+          position: absolute;
+          right: 24px;
+        }
+        .reward_txt {
+          color: #d67900;
+          font-size: 28px;
+          line-height: 40px;
+          text-align: center;
         }
       }
       .interact-msg {
