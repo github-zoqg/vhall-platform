7142c4234a1216d34a6529c4b8388cfc98efdeb8
enlong.zhang <enlong.zhang@vhall.com>
2022-08-09 16:33:58 +0800

    feat: 更换为dayjs

--- src/saas-watch/webpack.base.config.js
+++ src/saas-watch/webpack.base.config.js
@@ -13,10 +13,12 @@ const htmlConfig = {
     lodash: '//s1.e.vhall.com/common-static/middle/lodash/4.17.21/lodash.min.js',
     vue: '//s1.e.vhall.com/common-static/middle/vue/2.6.14/dist/vue.min.js',
     VueRouter: '//s1.e.vhall.com/common-static/middle/vue-router/3.5.3/dist/vue-router.min.js',
-    Moment: '//s1.e.vhall.com/common-static/middle/moment/2.24.0/moment.min.js',
+    // Moment: '//s1.e.vhall.com/common-static/middle/moment/2.24.0/moment.min.js',
     VueI18n: '//s1.e.vhall.com/common-static/middle/vue-i18n/8.26.7/vue-i18n.min.js',
     loadJs: '//s1.e.vhall.com/common-static/middle/loadjs/4.2.0/loadjs.min.js',
     ElementUi: '//s1.e.vhall.com/common-static/middle/element-ui/lib/2.6.2/index.js',
+    dayjs: '//s2.e.vhall.com/common-static/middle/dayjs/1.10.8/dayjs.min.js',
+    duration: '//s2.e.vhall.com/common-static/middle/dayjs/1.10.8/plugins/duration.js',
 
     // TODO: 图片验证码涉及到的地方，需引入该文件，如何放置
     imageYunDun: '//cstaticdun.126.net/load.min.js?t=201903281201',
