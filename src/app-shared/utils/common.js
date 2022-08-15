561877f538c38a557a5aa5bd60169634d3796ef3
enlong.zhang <enlong.zhang@vhall.com>
2022-08-15 13:33:27 +0800

    feat: 方法容错

--- src/app-shared/utils/common.js
+++ src/app-shared/utils/common.js
@@ -1,6 +1,6 @@
 //图片经阿里云转换
 export function resizeImg(url, { type, width, height }) {
-  if (url.indexOf('?x-oss-process=image/resize') < 0) {
+  if (url && url.indexOf('?x-oss-process=image/resize') < 0) {
     url += `?x-oss-process=image/resize,w_${width},h_${height},${type}`;
   }
   return url;
