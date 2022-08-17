cf9803f183d5e98d5dff08cbcd5dd3d21c28eddd
xushuo <893804934@qq.com>
2022-08-17 16:42:22 +0800

    feat: 完善主题换肤

--- src/app-shared/skins/watch/theme-simple-golden.js
+++ src/app-shared/skins/watch/theme-simple-golden.js
@@ -16,6 +16,7 @@ const color_text_placeholder = 'rgba(255, 255, 255, 0.35)';
 const color_text_disabled = 'rgba(255, 255, 255, 0.25)';
 const color_text_link = '#3562fa';
 const color_text_regular_2 = 'rgba(0, 0, 0, 0.65)';
+const color_text_1 = 'rgba(255, 255, 255, 0.85)';
 /* 背景颜色
 --------------------- */
 const color_background_0 = '#1a1a1a';
@@ -33,6 +34,17 @@ const color_background_11 = '#383838';
 const color_background_12 = '#e6e6e6';
 const color_background_13 = '#999';
 const color_background_14 = '#ccc';
+const color_background_15 = '#434343';
+const color_background_16 = '#ababab';
+const color_background_17 = '#e2e2e2';
+const color_background_18 = '#969696';
+const color_background_19 = '#595959';
+const color_background_20 = 'rgb(117, 117, 117)';
+const color_background_21 = '#666';
+const color_background_22 = 'rgba(255, 255, 255, 0.9)';
+const color_background_23 = '#34363a';
+const color_background_24 = '#23232d';
+const color_background_25 = '#777';
 
 export default {
   '--theme-background-color': color_background_0,
@@ -69,6 +81,33 @@ export default {
   //主题色
   '--theme-color': color_text_custom,
   '--theme-color-sub': color_text_custom_sub,
+  '--theme-menu-bg': color_background_24,
+  //分组
+  '--group-name-icon-font-color': color_text_regular,
+  '--group-name-font-color': color_text_secondary,
+  '--group-item-hover-color': color_background_6,
+  '--group-turnOff-font-color': color_text_1,
+  '--group-more-font-color': color_text_1,
+  '--group-more-hover-color': color_background_15,
+  '--group-online-font-color': color_background_16,
+  '--group-online-icon-color': color_background_17,
+  '--group-refresh-font-color': color_background_16,
+  '--group-btn-bg-color': color_background_15,
+  '--group-btn-font-color': color_background_13,
+  '--group-btn-hover-color': color_background_18,
+  '--group-btn-hover-font-color': color_text_primary,
+  '--group-btn-active-color': color_background_19,
+  '--group-btn-active-font-color': color_background_12,
+  '--group-more-mute-font-color': color_background_12,
+  '--group-search-font-color': color_background_13,
+  '--group-search-bg-color': color_background_17,
+  '--group-search-placeholder-color': color_background_20,
+  '--group-search-input-font-color': color_background_21,
+  '--group-search-input-bg-color': color_background_22,
+  '--group-search-input-bg2-color': color_background_23,
+  //侧边栏
+  '--menu-item-font-color': color_background_13,
+  '--menu-item-font-disable-color': color_background_25,
   //菜单-内容
   //简介
   '--theme-tab-content-intro-font': color_text_primary,
--- src/packages/member-list/src/main.vue
+++ src/packages/member-list/src/main.vue
@@ -1850,6 +1850,12 @@
 </script>
 
 <style lang="less">
+  .vmp-member-dropdown-menu.el-dropdown-menu {
+    background-color: var(--header-tab-item-dropdown-color) !important;
+    .el-dropdown-menu__item:hover {
+      color: var(--group-more-mute-font-color) !important;
+    }
+  }
   .vmp-member-list {
     height: 100%;
     display: flex;
@@ -1860,7 +1866,7 @@
       display: flex;
       align-items: center;
       padding: 18px 20px 5px;
-      color: #ccc;
+      color: var(--group-name-icon-font-color);
       .pr_top {
         margin-left: 10px;
         font-size: 14px;
@@ -1929,27 +1935,28 @@
       width: 100%;
       height: 80px;
       padding: 10px;
-      background-color: #2a2a2a;
+      background-color: var(--header-background-color-base);
       box-sizing: border-box;
       color: #e2e2e2;
-      border-top: 1px solid #1a1a1a;
+      border-top: 1px solid var(--header-background-color-base);
       .vh-saas-a-line-Onlinelist {
         margin-top: -3px;
         vertical-align: middle;
         margin-right: 4px;
+        color: var(--group-online-icon-color);
       }
       .info-panel__online-num {
         display: inline-block;
         margin-left: 6px;
-        color: #ababab;
+        color: var(--group-online-font-color);
       }
       .info-panel__refresh-btn {
         display: inline-block;
         margin-left: 6px;
-        color: #ababab;
+        color: var(--group-refresh-font-color);
         cursor: pointer;
         &:hover {
-          color: #4da1ff;
+          color: var(--theme-color);
         }
       }
       .info-panel__allow-raise-hand {
@@ -2011,23 +2018,23 @@
           width: 74px;
           height: 30px;
           text-align: center;
-          background-color: #434343;
-          color: #999;
+          background-color: var(--group-btn-bg-color);
+          color: var(--group-btn-font-color);
           float: left;
           cursor: pointer;
           position: relative;
           margin-right: 1px;
           &:hover {
-            background-color: #969696;
-            color: #fff;
+            background-color: var(--group-btn-hover-color);
+            color: var(--group-btn-hover-font-color);
           }
           &.active {
-            background-color: #595959;
-            color: #e6e6e6;
-            &:hover {
-              color: #e6e6e6;
-              background-color: #595959;
-            }
+            background-color: var(--group-btn-active-color);
+            color: var(--group-btn-active-font-color);
+            // &:hover {
+            //   color: #e6e6e6;
+            //   background-color: #595959;
+            // }
           }
           &.raise-hand {
             &::before {
@@ -2038,7 +2045,7 @@
               width: 7px;
               height: 7px;
               border-radius: 50%;
-              background-color: #fb3a32;
+              background-color: var(--theme-color);
             }
           }
         }
@@ -2049,8 +2056,8 @@
           line-height: 30px;
           border-radius: 4px;
           text-align: center;
-          color: #999;
-          background-color: #434343;
+          background-color: var(--group-search-bg-color);
+          color: var(--group-search-font-color);
         }
       }
       &__search-panel {
@@ -2060,7 +2067,7 @@
         position: absolute;
         top: 4px;
         left: 10px;
-        background-color: #34363a;
+        background-color: var(--group-search-input-bg2-color);
         border-radius: 4px;
         overflow: hidden;
 
@@ -2068,10 +2075,19 @@
           box-sizing: border-box;
           width: 100%;
           height: 100%;
-          background-color: hsla(0, 0%, 100%, 0.9);
+          background-color: var(--group-search-input-bg-color);
           padding: 0 75px 0 10px;
           border: none;
-          color: #666;
+          color: var(--group-search-input-font-color);
+          &::-webkit-input-placeholder {
+            color: var(--group-search-placeholder-color);
+          }
+          &::-moz-input-placeholder {
+            color: var(--group-search-placeholder-color);
+          }
+          &::-ms-input-placeholder {
+            color: var(--group-search-placeholder-color);
+          }
         }
         .search-panel__clear-btn {
           width: 15px;
@@ -2090,8 +2106,8 @@
           line-height: 30px;
           border-radius: 0 4px 4px 0;
           text-align: center;
-          background-color: #a6a6a8;
-          color: #fff;
+          background-color: var(--group-search-bg-color);
+          color: var(--group-search-font-color);
           position: absolute;
           top: 0;
           right: 0;
