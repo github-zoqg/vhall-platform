a5ba7251544142185ab0688654455abadc82252d
xushuo <893804934@qq.com>
2022-08-22 12:04:47 +0800

    fix: 问卷色值补全

--- src/app-shared/skins/wap/theme-concise-golden.js
+++ src/app-shared/skins/wap/theme-concise-golden.js
@@ -6,6 +6,7 @@ export default {
   '--theme-chat-input-bg': 'rgba(0, 0, 0, 0.1)',
   '--theme-header-attention-bg': 'linear-gradient(280.56deg, #C99F67 9.45%, #E5CFAE 90.39%)',
   '--theme-header-attention-color': 'rgba(82, 57, 50, 1)',
+  '--theme-component-qa': '#c99f67',
   //冗余
   '--theme-tab-menu-font': ' ',
   '--theme-tab-menu-font-active': ' ',
@@ -68,7 +69,6 @@ export default {
   '--theme-tab-content-private-name-font': ' ',
   '--theme-tab-content-private-content-font': ' ',
   '--theme-tab-content-private-time-font': ' ',
-  '--theme-component-qa': ' ',
   '--theme-component-subscribe': ' ',
   '--theme-component-subscribe-text': ' ',
   '--theme-component-subscribe-num': ' ',
