4c37f76dceecbc8e0d89146744c322362e93e881
xiaodong.ding <xiaodong.ding@vhall.com>
2022-09-05 12:03:36 +0800

    fix: cssVars 自动识别是否转换

--- src/app-shared/skins/skins-utils.js
+++ src/app-shared/skins/skins-utils.js
@@ -39,6 +39,6 @@ export const setTheme = (themeMap, el) => {
   cssVars({
     watch: true, // 添加、删除、修改 <link> 或 <style> 元素的禁用或 href 属性时，ponyfill 将自行调用
     variables: themeMap, // variables 自定义属性名/值对的集合
-    onlyLegacy: false // false 默认将 css 变量编译为浏览器识别的 css 样式 : true 当浏览器不支持css变量的时候将css变量编译为识别的css
+    onlyLegacy: true // false 默认将 css 变量编译为浏览器识别的 css 样式 : true 当浏览器不支持css变量的时候将css变量编译为识别的css
   });
 };
