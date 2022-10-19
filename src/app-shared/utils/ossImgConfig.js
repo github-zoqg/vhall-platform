b40405cb0dedde1594cd771dba158379b7e83092
Odyssey <qingzhao.xu@vhall.com>
2022-10-19 18:34:23 +0800

    fix: conflict

--- src/app-shared/utils/ossImgConfig.js
+++ src/app-shared/utils/ossImgConfig.js
@@ -1,6 +1,3 @@
-
-
-
-const BASE_PATH = process.env.VUE_APP_STATIC_BASE + '/common-static/images/'
+const BASE_PATH = process.env.VUE_APP_STATIC_BASE + '/common-static/images/';
 
 export const defaultAvatar = `${BASE_PATH}default_avatar.png`;
