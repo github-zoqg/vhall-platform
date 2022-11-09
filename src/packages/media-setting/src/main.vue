e1b6d08f19ec980fdcee649adb265b082badd670
xushuo <893804934@qq.com>
2022-11-09 11:10:11 +0800

    fix: 摄像头切换问题

--- src/packages/media-setting/src/main.vue
+++ src/packages/media-setting/src/main.vue
@@ -316,9 +316,9 @@
       async closeMediaSetting() {
         this.isShow = false;
         // TODO: 需要等待SDK更新，SDK销毁流异步处理有问题。
-        this.timeoutCache = setTimeout(() => {
-          this?.$refs['videoSetting']?.destroyStream();
-        }, 5000);
+        // this.timeoutCache = setTimeout(() => {
+        this?.$refs['videoSetting']?.destroyStream();
+        // }, 5000);
         this?.$refs['audioOutSetting']?.pauseAudio();
         this.isRepublishMode = false;
         await this.$nextTick();
