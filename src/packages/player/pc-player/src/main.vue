a9c9aa7b02ca3cd60514307524f16b18e28cef88
xiaodong.ding <xiaodong.ding@vhall.com>
2022-11-14 11:37:10 +0800

    fix: 播放器跑马灯文本为空同错处理

--- src/packages/player/pc-player/src/main.vue
+++ src/packages/player/pc-player/src/main.vue
@@ -565,8 +565,14 @@
               if (join_info.join_id) {
                 text = `${join_info.join_id}-${join_info.nickname}`;
               } else {
-                if (localStorage.getItem('userInfo')) {
-                  text = localStorage.getItem('userInfo').nick_name;
+                let userInfo = localStorage.getItem('userInfo');
+                if (userInfo) {
+                  try {
+                    userInfo = JSON.parse(localStorage.getItem('userInfo'));
+                    text = `${userInfo?.user_id}-${userInfo?.nick_name}`;
+                  } catch (err) {
+                    console.log(err);
+                  }
                 } else {
                   text = '';
                 }
@@ -575,7 +581,7 @@
             }
             params.marqueeOption = {
               enable: true,
-              text: marqueeText, // 跑马灯的文字
+              text: Boolean(marqueeText), // 跑马灯的文字
               alpha: this.marquee.alpha, // 透明度,100完全显示,0 隐藏
               size: this.marquee.size, // 文字大小
               color: this.marquee.color, // 文字颜色
