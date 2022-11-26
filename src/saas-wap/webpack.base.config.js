8cdcbc75fd396419af39b2e3a43d8e7ee36501c9
enlong.zhang <enlong.zhang@vhall.com>
2022-11-26 17:08:17 +0800

    feat: 修改表单sdk路径

--- src/saas-wap/webpack.base.config.js
+++ src/saas-wap/webpack.base.config.js
@@ -35,7 +35,7 @@ const htmlConfig = {
     MiddleEventSdk: '//s2.e.vhall.com/common-static/middle/middle-event-sdk/0.3.3/index.js',
     MiddleDomain: '',
     VhallUI: '//s2.e.vhall.com/common-static/ui/vhall-ui-lib/vh/1.1.1/index.js', // vhallUI
-    form: 'http://localhost:8000/vhall-form-vue.js'
+    form: 'https://vhallstatic.oss-cn-beijing.aliyuncs.com/common-static/vhall-form/vhall-form-vue.js'
   },
   // cdn css
   cdnCss: {
