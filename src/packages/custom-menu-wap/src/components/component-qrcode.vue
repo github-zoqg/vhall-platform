b9f03ca0dc797bbe0b45e4854c31bb19cf0bafd2
qiang.zhao <qiang.zhao@vhall.com>
2022-12-20 17:01:36 +0800

    feat: 二维码长度修改

--- src/packages/custom-menu-wap/src/components/component-qrcode.vue
+++ src/packages/custom-menu-wap/src/components/component-qrcode.vue
@@ -57,8 +57,8 @@
         text-align: center;
         img {
           display: inline-block;
-          width: 100%;
-          height: 100%;
+          width: 70%;
+          // height: 100%;
         }
       }
     }
--- src/packages/custom-menu/src/components/component-qrcode.vue
+++ src/packages/custom-menu/src/components/component-qrcode.vue
@@ -49,8 +49,8 @@
         text-align: center;
         img {
           display: inline-block;
-          width: 100%;
-          height: 100%;
+          width: 70%;
+          // height: 100%;
         }
       }
     }
